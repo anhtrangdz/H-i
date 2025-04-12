fe2a3e58b7f0cf7fb29a564b3bd977eeeee4443e
anhtrangdz <166999301+anhtrangdz@users.noreply.github.com>
2025-04-12 23:09:20 +0700

    Create Nettruyen.js

---
+++ Nettruyen.js
@@ -0,0 +1,17 @@
+{
+  "name": "NetTruyenVie",
+  "version": "1.0",
+  "lang": "vi",
+  "nsfw": false,
+  "overrideVersion": true,
+  "sources": [
+    {
+      "name": "NetTruyenVie (WebView)",
+      "id": "nettruyenvie-webview",
+      "baseUrl": "https://nettruyenvie.com/amp",
+      "lang": "vi",
+      "isNsfw": false,
+      "supportsWebView": true
+    }
+  ]
+}
